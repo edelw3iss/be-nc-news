dde9fb0b729d987ee3807c9b2cac32a19e6a1b44
edelw3iss <eloisemoore1981@yahoo.co.uk>
2022-02-18 14:16:17 +0000

    refactored to require in endpoints, ticket 13

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const data = require("../db/data/test-data");
 const app = require("../app");
 const seed = require("../db/seeds/seed");
 const connection = require("../db/connection");
+const endpoints = require('../endpoints.json')
 
 beforeEach(() => seed(data));
 afterAll(() => connection.end());
@@ -453,113 +454,11 @@ describe("news-app", () => {
   describe("/api", () => {
     describe("GET", () => {
       test("status: 200 - responds with endpoints JSON", () => {
-        const expected = {
-          "GET /api": {
-            "description": "serves up a json representation of all the available endpoints of the api"
-          },
-          "GET /api/topics": {
-            "description": "serves an array of all topics",
-            "queries": [],
-            "exampleResponse": {
-              "topics": [{ "slug": "football", "description": "Footie!" }]
-            }
-          },
-          "GET /api/articles": {
-            "description": "serves an array of all topics",
-            "queries": ["topic", "sort_by", "order_by"],
-            "exampleResponse": {
-              "articles": [
-                {
-                  "article_id": 34,
-                  "title": "Seafood substitutions are increasing",
-                  "topic": "cooking",
-                  "author": "weegembump",
-                  "created_at": "2021-01-01T12:00:00Z",
-                  "votes": 64,
-                  "comment_count": 5
-                }
-              ]
-            }
-          },
-          "GET /api/articles/:article_id": {
-            "description": "serves an article object",
-            "queries": [],
-            "exampleResponse": {
-              "article": {
-                "article_id": 2,
-                "title": "Seafood substitutions are increasing",
-                "topic": "cooking",
-                "author": "weegembump",
-                "body": "Text from the article..",
-                "created_at": "2021-01-01T12:00:00Z",
-                "votes": 20,
-                "comment_count": 4
-              }
-            }
-          },
-          "PATCH /api/articles/:article_id": {
-            "description": "allows user to update votes on an article and serves an update article object",
-            "exampleInput": { "inc_votes": 10 },
-            "exampleResponse": {
-              "article": {
-                "article_id": 2,
-                "title": "Seafood substitutions are increasing",
-                "topic": "cooking",
-                "author": "weegembump",
-                "body": "Text from the article..",
-                "created_at": "2021-01-01T12:00:00Z",
-                "votes": 30
-              }
-            }
-          },
-          "GET /api/articles/:article_id/comments": {
-            "description": "serves an array of comments for specified article",
-            "queries": [],
-            "exampleResponse": {
-              "comments": [
-                {
-                  "comment_id": 23,
-                  "votes": 3,
-                  "created-at": "2021-01-01T12:00:00Z",
-                  "author": "poppy_petal",
-                  "body": "This article was ..."
-                }
-              ]
-            }
-          },
-          "POST /api/articles/:article_id/comments": {
-            "description": "allows user to post a comment for an article and serves an object with the posted comment",
-            "exampleInput": {
-              "username": "poppy_petal",
-              "body": "This article is ..."
-            },
-            "exampleResponse": {
-              "comment": {
-                "comment_id": 23,
-                "votes": 3,
-                "created-at": "2021-01-01T12:00:00Z",
-                "author": "poppy_petal",
-                "body": "This article was ..."
-              }
-            }
-          },
-          "DELETE /api/comments/:comment_id": {
-            "Description": "deletes specified comment"
-          },
-          "GET /api/users": {
-            "description": "serves an array of all users",
-            "queries": [],
-            "exampleResponse": {
-              "users": [{ "username": "poppy_petal" }, { "username": "violet22" }]
-            }
-          }
-        }
-        
         return request(app)
           .get("/api")
           .expect(200)
           .then(({ body }) => {
-            expect(body).toEqual(expected);
+            expect(body).toEqual(endpoints);
           });
       });
     });

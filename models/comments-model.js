7bfe0e2db8d210b30f041a3634517bff578cb7fd
Eloise Chilvers <eloisechilvers@Eloises-MBP.home>
2022-03-16 08:45:29 +0000

    change fetch comments to default to order by created-at desc

--- models/comments-model.js
+++ models/comments-model.js
@@ -4,7 +4,8 @@ exports.fetchCommentsByArticleId = (articleId) => {
   return db
     .query(
       `SELECT comment_id, votes, created_at, author, body FROM comments
-    WHERE article_id = $1;`,
+    WHERE article_id = $1
+    ORDER BY created_at desc;`,
       [articleId]
     )
     .then(({ rows }) => {
